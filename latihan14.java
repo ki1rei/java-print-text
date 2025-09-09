beea3a7246e809ca0d8ad31a721834a41b143adb
ki1rei <studyacctyara@gmail.com>
2025-09-09 07:28:20 +0700

    first commit

---
+++ latihan14.java
@@ -0,0 +1,12 @@
+/**
+ * latihan14
+ */
+public class latihan14 {
+    public static void main(String[] args) {
+     System.out.println("Halo Nama Saya Firstyara Putri ");
+     
+     System.out.println("Belajar Java itu Mudah!");
+     System.out.println("Semakin sering mencoba, semakin jago");   
+    }
+}
+
